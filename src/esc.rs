d470dd947dcd4cbb9d26061c4b2cf3ba895ee4fa
Vitaly Domnikov <dotcypress@gmail.com>
2022-12-08 19:43:54 -0800

    cleanup

--- src/esc.rs
+++ src/esc.rs
@@ -5,14 +5,14 @@ use hal::timer::*;
 use crate::*;
 
 pub struct MotorControl {
-    pub enable: MotorDriverEnable,
-    pub fault: MotorDriverFault,
-    pub tork: PwmPin<TIM16, Channel1>,
-    pub pwm_tim: Pwm<TIM1>,
-    pub pwm_a: PwmPin<TIM1, Channel4>,
-    pub pwm_b: PwmPin<TIM1, Channel1>,
-    pub phase_a: MotorAPhase,
-    pub phase_b: MotorBPhase,
+    enable: MotorDriverEnable,
+    fault: MotorDriverFault,
+    phase_a: MotorAPhase,
+    phase_b: MotorBPhase,
+    pwm_tim: Pwm<TIM1>,
+    pwm_a: PwmPin<TIM1, Channel4>,
+    pwm_b: PwmPin<TIM1, Channel1>,
+    tork: PwmPin<TIM16, Channel1>,
 }
 
 impl MotorControl {
@@ -31,14 +31,16 @@ impl MotorControl {
     ) -> Self {
         let tork_pwm = tork_tim.pwm(10.kHz(), rcc);
         let mut tork = tork_pwm.bind_pin(tork);
-        tork.set_duty(tork.get_max_duty());
+        let min_tork = 4_200;
+        let max_tork = 3_800;
+        tork.set_duty(3_900);
         tork.enable();
 
         let mut pwm_tim = speed_tim.pwm(100.Hz(), rcc);
-        // pwm_tim.listen();
+        pwm_tim.listen();
 
         let mut pwm_a = pwm_tim.bind_pin(pwm_a);
-        pwm_a.set_duty(0);
+        pwm_a.set_duty(pwm_a.get_max_duty()/8);
         pwm_a.enable();
 
         let mut pwm_b = pwm_tim.bind_pin(pwm_b);
@@ -55,5 +57,18 @@ impl MotorControl {
             phase_a,
             phase_b,
         }
+    }
+
+    pub fn tick(&mut self) {
+        self.pwm_tim.clear_irq();
+        if self.fault.is_low().unwrap() {}
+    }
+
+    pub fn on(&mut self) {
+        self.enable.set_high().ok();
+    }
+
+    pub fn off(&mut self) {
+        self.enable.set_low().ok();
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,11 @@ pub extern crate stm32g0xx_hal as hal;
 extern crate panic_halt;
 extern crate rtic;
 
-mod ir;
 mod esc;
+mod ir;
 mod pins;
-mod platform;
 
+use esc::*;
 use hal::analog::adc;
 use hal::gpio::SignalEdge;
 use hal::i2c;
@@ -23,9 +23,7 @@ use hal::spi;
 use hal::stm32;
 use hal::stm32::*;
 use ir::*;
-use esc::*;
 use pins::*;
-use platform::*;
 use smart_leds::*;
 use ws2812_spi as ws2812;
 
@@ -46,76 +44,93 @@ mod curio {
     #[init]
     fn init(ctx: init::Context) -> (Shared, Local, init::Monotonics) {
         defmt::info!("init");
+        // Configure APB bus clock to 48MHz, cause ws2812 requires 3Mbps SPI
+        let pll_cfg = PllConfig::with_hsi(4, 24, 2);
+        let rcc_cfg = rcc::Config::pll().pll_cfg(pll_cfg);
+        let mut rcc = ctx.device.RCC.freeze(rcc_cfg);
 
-        let mut platform = Platform::new(
-            ctx.device.RCC,
-            ctx.device.PWR,
-            ctx.device.EXTI,
-            ctx.device.ADC,
+        let pins = Pins::new(
             ctx.device.GPIOA,
             ctx.device.GPIOB,
             ctx.device.GPIOC,
-            ctx.device.TIM1,
-            ctx.device.TIM14,
-            ctx.device.TIM16,
-            ctx.device.TIM17,
-            ctx.device.SPI1,
-            ctx.device.I2C1,
-            i2c::Config::new(400.kHz()),
+            &mut rcc,
         );
 
-        platform.standby.set_high().ok();
+        let mut exti = ctx.device.EXTI;
+        let mut standby = pins.standby;
+        let wakeup = pins.wakeup_button.listen(SignalEdge::Falling, &mut exti);
 
-        platform.esc.enable.set_high().ok();
-        platform.esc.tork.set_duty(4000);
-        // platform
-        //     .esc
-        //     .pwm_a
-        //     .set_duty(platform.esc.pwm_a.get_max_duty() / 2);
-        // platform
-        //     .esc
-        //     .pwm_b
-        //     .set_duty(platform.esc.pwm_b.get_max_duty() / 2);
+        let battery_sense = pins.battery_sense;
+        let mut adc = ctx.device.ADC.constrain(&mut rcc);
+        adc.set_sample_time(adc::SampleTime::T_80);
+        adc.set_precision(adc::Precision::B_12);
 
-        let mut delay = ctx.core.SYST.delay(&mut platform.rcc);
-        let mut cnt: usize = 0;
+        let mut ir_tim = ctx.device.TIM17.pwm(IR_TIMER_FREQUENCY.Hz(), &mut rcc);
+        ir_tim.listen();
+
+        let ir_carrier_tim = ctx.device.TIM14.pwm(IR_CARRIER_FREQUENCY.Hz(), &mut rcc);
+        let rx = pins.ir_rx.listen(SignalEdge::All, &mut exti);
+        let ir = IrTransceiver::new(ir_tim, ir_carrier_tim, pins.ir_tx, rx);
+
+        let mut pwr = ctx.device.PWR.constrain(&mut rcc);
+        pwr.clear_standby_flag();
+        pwr.enable_wakeup_lane(WakeUp::Line1, SignalEdge::Falling);
+
+        let i2c = ctx.device.I2C1.i2c(
+            pins.i2c_sda,
+            pins.i2c_clk,
+            i2c::Config::new(400.kHz()),
+            &mut rcc,
+        );
+
+        let spi = ctx.device.SPI1.spi(
+            (spi::NoSck, spi::NoMiso, pins.neopixel),
+            ws2812::MODE,
+            3.MHz(),
+            &mut rcc,
+        );
+        let mut neopixel = ws2812::Ws2812::new(spi);
+
         let mut data: [RGB<u8>; 10] = [RGB::default(); 10];
-        loop {
-            for (idx, c) in data.iter_mut().enumerate() {
-                *c = RGB {
-                    r: 24 - ((cnt + idx) % 24) as u8,
-                    g: 0, //(255 - cnt) as u8,
-                    b: 0, //((cnt + idx) % 24) as u8,
-                };
-            }
-            platform.neopixel.write(data.iter().cloned()).unwrap();
-            cnt += 1;
-            delay.delay(20.millis());
+        for (idx, c) in data.iter_mut().enumerate() {
+            *c = RGB {
+                r: idx as _,
+                g: 0,
+                b: 0,
+            };
         }
+        neopixel.write(data.iter().cloned()).unwrap();
+
+        standby.set_high().ok();
+        let mut esc = MotorControl::new(
+            ctx.device.TIM1,
+            ctx.device.TIM16,
+            pins.motor_driver_tork,
+            pins.motor_driver_enable,
+            pins.motor_driver_fault,
+            pins.motor_a_pwm,
+            pins.motor_b_pwm,
+            pins.motor_a_phase,
+            pins.motor_b_phase,
+            &mut rcc,
+        );
+
+        esc.on();
 
         defmt::info!("init done");
-
-        (
-            Shared {
-                ir: platform.ir,
-                exti: platform.exti,
-                esc: platform.esc,
-            },
-            Local {},
-            init::Monotonics(),
-        )
+        (Shared { ir, exti, esc }, Local {}, init::Monotonics())
     }
 
-    #[task(binds = TIM1_BRK_UP_TRG_COM, shared = [])]
-    fn esc_tick(_ctx: esc_tick::Context) {
-        defmt::info!("esc");
+    #[task(binds = TIM1_BRK_UP_TRG_COM, shared = [esc])]
+    fn esc_tick(ctx: esc_tick::Context) {
+        let mut esc = ctx.shared.esc;
+        esc.lock(|esc| esc.tick());
     }
 
     #[task(binds = EXTI0_1, shared = [exti])]
     fn wakeup(ctx: wakeup::Context) {
         let mut exti = ctx.shared.exti;
         exti.lock(|exti| exti.unpend(hal::exti::Event::GPIO0));
-        defmt::info!("wakeup");
     }
 
     #[task(binds = TIM17, shared = [ir])]
@@ -124,74 +139,13 @@ mod curio {
         ir.lock(|ir| ir.tick());
     }
 
-    #[task(binds = EXTI4_15, shared = [ir, exti, esc])]
+    #[task(binds = EXTI4_15, shared = [ir, exti])]
     fn ir_rx(ctx: ir_rx::Context) {
         let mut exti = ctx.shared.exti;
         let mut ir = ctx.shared.ir;
-        let mut esc = ctx.shared.esc;
         exti.lock(|exti| exti.unpend(hal::exti::Event::GPIO15));
         if let Ok(Some(cmd)) = ir.lock(|ir| ir.event()) {
             defmt::info!("ir: {:?}", cmd);
-            esc.lock(|esc| match cmd.cmd {
-                64 => {
-                    esc.pwm_a.set_duty(
-                        esc.pwm_a
-                            .get_duty()
-                            .saturating_add(2048)
-                            .min(esc.pwm_a.get_max_duty()),
-                    );
-                    esc.pwm_b.set_duty(
-                        esc.pwm_b
-                            .get_duty()
-                            .saturating_add(2048)
-                            .min(esc.pwm_b.get_max_duty()),
-                    );
-                }
-                25 => {
-                    esc.pwm_a
-                        .set_duty(esc.pwm_a.get_duty().saturating_sub(2048));
-                    esc.pwm_b
-                        .set_duty(esc.pwm_b.get_duty().saturating_sub(2048));
-                }
-                7 => {
-                    esc.pwm_a
-                        .set_duty(esc.pwm_a.get_duty().saturating_sub(2048));
-
-                    esc.pwm_b.set_duty(
-                        esc.pwm_b
-                            .get_duty()
-                            .saturating_add(2048)
-                            .min(esc.pwm_b.get_max_duty()),
-                    );
-                }
-                9 => {
-                    esc.pwm_a.set_duty(
-                        esc.pwm_a
-                            .get_duty()
-                            .saturating_add(2048)
-                            .min(esc.pwm_a.get_max_duty()),
-                    );
-                    esc.pwm_b
-                        .set_duty(esc.pwm_b.get_duty().saturating_sub(2048));
-                }
-                21 => {
-                    let duty = esc.pwm_a.get_duty() / 2 + esc.pwm_b.get_duty() / 2;
-                    esc.pwm_a.set_duty(duty);
-                    esc.pwm_b.set_duty(duty);
-                }
-                70 => {
-                    esc.pwm_a.set_duty(0);
-                    esc.pwm_b.set_duty(0);
-                }
-                _ => {}
-            })
-        }
-    }
-
-    #[idle]
-    fn idle(_: idle::Context) -> ! {
-        loop {
-            rtic::export::nop();
         }
     }
 }
